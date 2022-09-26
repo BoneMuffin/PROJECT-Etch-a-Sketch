e81fd2615133d2cf00e1c0d80591397703b34ca2
valen <valen.bagaglia@gmail.com>
2022-09-26 11:04:15 -0300

    created loop for grid 4x4

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,19 @@
-document.getElementById("container");
+const sizeOfGrid = 4;
+const resetButton = document.querySelector('button')
+const wrapper = document.querySelector(".wrapper");
+
+const createGrid = (amtOfGrids) => {
+    for (let i = 0; i < amtOfGrids; i ++ ) {
+        const row = document.createElement('div')
+        row.classList.add('grid-row')
+
+        for (let j = 0; j < amtOfGrids; j++) {
+            const gridBox = document.createElement('div')
+            gridBox.classList.add('grid-box')
+            row.appendChild(gridBox)
+        }
+        wrapper.appendChild(row)
+    }
+}
+
+createGrid(sizeOfGrid);
