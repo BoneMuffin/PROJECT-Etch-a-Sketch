263875e76bd71b756a2623c3addd3dc4048cf50f
smrtblkt <valen.bagaglia@gmail.com>
2022-11-10 14:29:01 -0600

    added loop to create grids

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,3 +2,17 @@ const sizeButton = document.querySelectorAll(".small.medium.big");
 const resetButton = document.querySelector('button');
 const grid = document.querySelector("#grid");
 
+const createGrid = (size = 32 * 44, cssClass = "medium-grid") => {
+    const gameWrapper = document.getElementById('grid');
+
+    gameWrapper.innerHTML = '';
+
+    gameWrapper.classList.remove("small-grid", "medium-grid", "big-grid");
+    gameWrapper.classList.add(cssClass);
+
+    for (let i = 0; i < size; i += 1) { // spawn default grid
+        const div = document.createElement('div');
+        gameWrapper.appendChild(div);
+    }
+}
+
