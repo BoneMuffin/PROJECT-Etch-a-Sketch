5119d3a8b4617cdbf0ed708cdb47832a1cc92bf5
smrtblkt <valen.bagaglia@gmail.com>
2022-11-10 15:45:49 -0600

    only god knows what happened here

---
+++ etch-a-sketch.js
@@ -0,0 +1,54 @@
+const resetButton = document.querySelector('button');
+const grid = document.querySelector('#grid');
+
+let currentColors = 'classic';
+
+const groovyColors = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+     return {r, g, b};
+}
+
+const createGrid = (amtOfGrids = 32 * 44, cssClass = 'medium-grid') => {
+    const gameWrapper = document.createElementById('grid');
+
+    gameWrapper.innerHtml = '';
+    gameWrapper.classList.remove("small-grid", "medium-grid", "big-grid");
+    gameWrapper.classList.add('cssClass');
+
+    for (let i = 0; i < amtOfGrids; i ++ ) {
+        const row = document.createElement('div');
+        gameWrapper.appendChild('row');
+    }
+}
+
+const erase = document.querySelector('.erase');
+    const gridItems = document.querySelectorAll('#grid-container > div');
+
+    gridItems.forEach((item) => {
+    const gridItem = item;
+    gridItem.style.backgroundColor = white;
+    gridItem.style.opacity = '1';
+    gridItem.count = 0;
+ });
+
+function painting(mode) {
+    const gridItems = document.querySelectorAll('#grid >div');
+
+    gridItems.forEach((item) => {
+        const gridItem = item;
+        gridItem.count = 0;
+        gridItem.addEventListener('mouseenter', (e) => {
+        if (mode === 'classic') {
+           
+            gridItem.addEventListener('mouseenter', () => {                
+                const opacity = Number(gridItem.style.opacity)
+                gridItem.style.background = `rgb(0, 0, 0)`
+                gridItem.style.opacity = opacity + .15
+        });
+        row.appendChild(gridItem);
+     }
+    });
+    grid.appendChild(gridItems);
+ };
