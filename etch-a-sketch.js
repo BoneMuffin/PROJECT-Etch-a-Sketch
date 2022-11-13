eb74c64e740e14d51c82d687131e9733102ae2df
smrtblkt <valen.bagaglia@gmail.com>
2022-11-13 15:10:05 -0600

    only god knows what's going on

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,62 +1,16 @@
-// const sizeButtons = document.querySelectorAll('.size');//
-const eraseButton = document.querySelector('.erase');
-const colorButtons = document.querySelectorAll('.mode');
-const container = document.getElementById('grid-wrapper')
+const sizeButtons = document.querySelector('.size');
+const modeButtons = document.querySelectorAll('mode');
+const eraseButton = document.querySelector('#erase-button'); 
+const container = document.querySelector('.container');
 
-let currentColors = 'classicMode';
 
-const createGrid = (amtOfGrids) => {
-  const wrapper = document.createElement('div')
-  wrapper.classList.add('wrapper')
+function erase() {
+  const gridItems = document.querySelectorAll('#grid-container > div');
 
-  for (let i = 0; i < amtOfGrids; i++) {
-    const row = document.createElement('div')
-    row.classList.add('grid-row')
-
-    for (let j = 0; j < amtOfGrids; j++) {
-      const widthAndHeight = 960 / amtOfGrids
-      const gridBox = document.createElement('div')
-      gridBox.classList.add('grid-box')
-      gridBox.style.width = `${widthAndHeight}px`
-      gridBox.style.height = `${widthAndHeight}px`
-      // adding mouseenter listener to change background color
-      gridBox.addEventListener('mouseenter', () => {
-        const classicMode = Number(gridBox.style.opacity)
-        gridBox.style.background = `rgb(0, 0, 0)`
-        gridBox.style.opacity = classicMode + .1
-      })
-      row.appendChild(gridBox)
-    }
-
-    wrapper.appendChild(row)
-  } 
-  container.appendChild(wrapper)
+  gridItems.forEach((item) => {
+    const gridItem = item;
+    gridItem.style.backgroundColor = '#D8D8D8';
+    gridItem.style.opacity = '1';
+    gridItem.count = 0;
+  });
 }
-
-
-const groovyMode = () => {
-  const r = Math.floor(Math.random() * 256)
-  const g = Math.floor(Math.random() * 256)
-  const b = Math.floor(Math.random() * 256)
-
-  return { r, g, b }
-}
-
-
-eraseButton.addEventListener('click', () => {
-  let userSize = Number(prompt('what dimensions do you want for the new grid'))
-
-  while (userSize > 100) {
-    userSize = Number(prompt('Pick a smaller number and make sure its 100 or less'))
-  }
-
-  const wrapper = document.querySelector('.wrapper')
-
-  if (!wrapper) {
-    createGrid(userSize)
-  } else {
-    wrapper.remove()
-    createGrid(userSize)
-  }
-
-})
