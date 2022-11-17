2dc47d6f0080814b5c35b60a6cb43f6e4cbf3c09
smrtblkt <valen.bagaglia@gmail.com>
2022-11-17 09:47:39 -0600

    only god knows what's going on here

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,39 +1,62 @@
-const sizeButtons = document.querySelectorAll('.size');
+const sizeButton = document.querySelectorAll('.size-selection');
 const modeButtons = document.querySelectorAll('.mode');
 const eraseButton = document.querySelector('#erase-button'); 
 
-const canvasContainer = document.querySelector('#canvas-container');
+const container = document.querySelector('#canvas-container');
 const redFrame = document.querySelector('.red-frame');
 
 
 // 16x16 default grid inside grid wrapper (960px wide), fully functioning for mouse events
 const createGrid = (amtOfGrids) => {
-    const canvasWrapper = document.createElement('div')
-    canvasWrapper.classList.add('canvas-container')
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
+  
+    for (let i = 0; i < amtOfGrids; i++) {
+      const row = document.createElement('div')
+      row.classList.add('grid-row')
+  
+      for (let j = 0; j < amtOfGrids; j++) {
+        const widthAndHeight = 960 / amtOfGrids
+        const gridBox = document.createElement('div')
+        gridBox.classList.add('grid-box')
+        gridBox.style.width = `${widthAndHeight}px`
+        gridBox.style.height = `${widthAndHeight}px`
+        // adding mouseenter listener to change background color
+        gridBox.addEventListener('mouseenter', () => {
+          const classicMode = Number(gridBox.style.opacity)
+          gridBox.style.background = `rgb(0, 0, 0)`
+          gridBox.style.opacity = classicMode + .1
+        })
+        row.appendChild(gridBox)
+      }
 
-    for (let i = 0; i < amtOfGrids; i++) {    // layout of the grid cells
-        const row = document.createElement('div') 
-        row.classLisst.add('grid-row')
+      container.appendChild(row)
+    } 
+     container.appendChild(wrapper)
+  }
+  
 
-        for (let j = 0; j < amtOfGrids; j++) {
-            const widthAndHeight = 1080 / amtOfGrids
-            const gridBox = document.createElement('div')
-            gridBox.classList.add('grid-box')    // so the boxes have grey borders :p
-            gridBox.style.width = `${widthAndHeight}px` // result of division into px
-            gridBox.style.height = `${widthAndHeight}px`
-        }
+sizeButton.addEventListener('click', () => {
+    let userSize = Number(prompt('what dimensions do you want for the new grid'))
+  
+    while (userSize > 100) {
+      userSize = Number(prompt('Pick a smaller number and make sure its 100 or less'))
     }
-}
+  
+    const wrapper = document.querySelector('.wrapper')
+  
+    if (!wrapper) {
+      createGrid(userSize)
+    } else {
+      wrapper.remove()
+      createGrid(userSize)
+    }
+  
+  })
+
 // groovy(rgb) and classic mode(+10% opacity with each mouseover), functioning buttons
-const paintDivs =
+//const paintDivs =
 // fully functioning size buttons
 
 // add class for the shaking animation //
-function shakeCanvas() {
-    const squares = document.querySelectorAll('.grid-square');
-    squares.forEach(square => square.style.backgroundColor = "");
-    appContainer.classList.add('canvas-shake');
-    //when the animation is finished, remove the class so it's ready to run again
-    appContainer.addEventListener('animationend',() => appContainer.classList.remove('canvas-shake'));
-  };
-  
+
